7141abc3e4174c128df1ea7d6c56a20d83626393
Sage Beard <sagek711@gmail.com>
2017-01-07 21:44:46 -0800

    attempt 2

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -19,29 +19,28 @@
 function BlocChatCookies($cookies, $uibModal) {
   var currentUser = $cookies.get('blocChatCurrentUser');
   if (!currentUser || currentUser === '') {
-    $cookies.put('blocChatCurrentUser', 'username');
      var modalInstance = $uibModal.open({
         ariaLabelledBy: 'modal-title',
         ariaDescribedBy: 'modal-body',
         templateUrl: '/templates/nameModal.html',
-        controllerAs: '$ctrl',
         controller: 'EnterUserNameCtrl',
+        controllerAs: '$ctrl',
         backdrop: 'static',
         keyboard: false
      });
   }
-}
 
     function EnterUserNameCtrl($cookies, $uibModalInstance) {
-
         this.setUsername = function(userName) {
-          // set new cookie here
+           $cookies.put('blocChatCurrentUser', 'userName');// set new cookie here
+           $uibModalInstance.dismiss('cancel');
         };
     }
 }
+ 
 angular
     .module('blocChat', ['ui.router', 'ui.bootstrap', 'firebase', 'ngCookies'])
-    .contoller('EnterUserNameCtrl', ['$cookies', '$uibModalInstance', EnterUserNameCtrl])
+    .controller('EnterUserNameCtrl', ['$cookies', '$uibModalInstance', EnterUserNameCtrl])
     .config(config)
     .run(['$cookies', '$uibModal', BlocChatCookies]);
 
